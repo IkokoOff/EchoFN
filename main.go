9bd5688672869dc34ec1622094656c8fdc34494d
IkokoOff <159687221+IkokoOff@users.noreply.github.com>
2024-05-01 20:55:13 +0200

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/goccy/go-json"
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/master"
 )
 
 //go:embed config.ini
